779157172b8a427a2359824b4cf4e8bd63b52bc5
Jerry bony <16891252+Jbony1988@users.noreply.github.com>
2023-03-10 20:44:14 -0600

    commit
    commit

---
+++ Angular/my-first-project/src/app/components/todo/todo.component.ts
@@ -0,0 +1,49 @@
+import { Component } from '@angular/core';
+import {Todo} from "../../models/Todo";
+
+@Component({
+  selector: 'app-todo',
+  templateUrl: './todo.component.html',
+  styleUrls: ['./todo.component.css']
+})
+export class TodoComponent {
+
+todos!:Todo[];
+inputTodo: string = "";
+constructor() {}
+ngOnInit(): void {
+  this.todos = [
+  {
+    content: "First Content",
+    completed: false
+  },
+  {
+    content: "Second Content",
+    completed: false
+  }
+]
+}
+toggleDone(id:number) {
+this.todos.map((v, i) => {
+  if(i === id)  v.completed = !v.completed
+  return v
+   
+  
+})
+}
+
+deleteTodo(id:number) {
+  console.log(id)
+this.todos = this.todos.filter((v, i) => i !== id);
+}
+
+
+addTodo () {
+  this.todos.push({
+    content: this.inputTodo,
+    completed: false
+  })
+  this.inputTodo = ""
+}
+
+}
